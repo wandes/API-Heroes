623179808b8c31941c0dfd6e898a44af2af20e94
Wandes Gomes <wandessilva@outlook.com>
2019-06-22 01:04:32 -0300

    configurando porta no heroku

--- bin/server.js
+++ bin/server.js
@@ -2,32 +2,14 @@ const app = require('../src/app')
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 const cors = require('cors')
-const http_port = 8080;
 
-const port =  normalizePort(process.env.PORT || http_port);
-
-app.set('port', port);
-
-//Normalizando a porta
-function normalizePort(val) {
-  
-    var port = parseInt(val, 10);
-
-        if (isNaN(port)) {
-
-            return val;
-        }
-
-        if (port >= 0) {
-           
-            return port;
-        }
-
-        return false;
-}
 
 const server = http.createServer(app);
 
+const port = process.env.PORT || 3000;
+
 server.listen(port);
+
 app.use(cors())
+
 console.log('API rodando na porta ' + port);
